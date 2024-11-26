7d9408586c8085101c4802d9d7972f741e1e3dce
Md. Sohanur Rahman Sakib <hello@sakibsti.me>
2024-11-26 16:39:52 +0600

    Create local_rememberme.php

---
+++ rememberme/lang/en/local_rememberme.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'Remember Me';
+$string['rememberme'] = 'Remember Me';
+$string['rememberme_desc'] = 'Stay logged in for a configurable period.';
+$string['token_lifetime'] = 'Token lifetime (in seconds)';
+$string['token_lifetime_desc'] = 'Duration for which the "Remember Me" token is valid.';
