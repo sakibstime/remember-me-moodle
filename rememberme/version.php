52da727bfc2fd109aa582bdb00f6a775a8c957a9
Md. Sohanur Rahman Sakib <hello@sakibsti.me>
2024-11-26 16:39:30 +0600

    Create version.php

---
+++ rememberme/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_rememberme';
+$plugin->version = 2024112600;  // YYYYMMDDXX format.
+$plugin->requires = 2021051700; // Moodle 3.11 or later.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
